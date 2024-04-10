ade409583d5902b7f9ebd293fdaa1c4e3db9d8b6
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-10 20:36:12 +0400

    Component Selectors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  // selector: 'app-root',
+  // selector: '.app-root',
+  // selector: '#app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
