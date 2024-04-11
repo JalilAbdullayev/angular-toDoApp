bca4b899418cbea0f89d7374b7d92821201731c9
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-11 20:14:50 +0400

    Styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,11 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app',
-  // templateUrl: './app.component.html',
-  template: `<h1>app component</h1>
-  <h2>{{ title }}</h2>
-  <h2>{{ getTitle() }}</h2>
-  <p>
-    {{ toDoItem.description }}
-    {{ toDoItem.action }}
-  </p>
-  `,
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html',
+  // styleUrl: './app.component.css'
+  styles: [`p {
+    color: red;
+  }`]
 })
 export class AppComponent {
   title = 'to Do App';
