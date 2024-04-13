afc5e8c7767cb279751c2250ce20b8212dac7326
JalilAbdullayev <abdullacelil00@gmail.com>
2024-04-13 16:52:08 +0400

    Lists

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -6,5 +6,22 @@ import {Component} from '@angular/core';
   styleUrl: './todo.component.css'
 })
 export class TodoComponent {
+  private name = 'Jalil';
+  items = [{
+    id: 1,
+    description: 'Breakfast',
+    action: 'yes'
+  }, {
+    id: 2,
+    description: 'Workout',
+    action: 'yes'
+  }, {
+    id: 3,
+    description: 'Shopping',
+    action: 'no'
+  }]
 
+  getName() {
+    return this.name;
+  }
 }
